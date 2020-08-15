737587d6335b9b75185d419a0a2b07f2278983a3
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-08-15 12:52:38 -0400

    ex2, ex4 completed. MVP

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,39 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+tickets = [
+    Ticket("PIT",  "ORD" ),
+    Ticket("XNA",  "CID" ),
+    Ticket("SFO",  "BHM"),
+    Ticket("FLG",  "XNA"),
+    Ticket("NONE",  "LAX"),
+    Ticket(  "LAX",  "SFO"),
+    Ticket(  "CID",  "SLC"),
+    Ticket(  "ORD",  "NONE"),
+    Ticket(  "SLC",  "PIT"),
+    Ticket(  "BHM",  "FLG")
+]
 
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    route = [None] * length
 
+    for i in range(length):
+
+        if tickets[i].source == "NONE":
+            route[0] = tickets[i].destination
+
+        if tickets[i].source not in cache:
+            cache[tickets[i].source] = tickets[i].destination
+
+    for x in range(length):
+        
+        if route[x - 1] is not None:
+            route[x] = cache[route[x - 1]]
     return route
+
+print(reconstruct_trip(tickets, 10))
