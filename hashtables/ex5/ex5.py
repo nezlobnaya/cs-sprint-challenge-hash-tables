15c52b1252a6c8120e0dd7419f926c1329653204
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-08-15 12:29:51 -0400

    First commit. ex5 completed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,8 +7,23 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    file_names = {}
+    paths = []
+    for file in files:
+        split_file = file.split("/")
+        file_name = split_file[-1]
+        if file_name not in file_names:
+            file_names.setdefault(file_name, [file])
+        else:
+            file_names[file_name].append(file)
+        
+    for query in queries:
+        if query in file_names:
+            for file in file_names[query]:
+                paths.append(file)    
+    return paths
 
-    return result
+    
 
 
 if __name__ == "__main__":
